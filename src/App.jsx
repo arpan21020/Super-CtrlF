1d8ad7f8ed843a1de36ed86b238b5c45ca3fce1b
arpan21020 <arpan21020@iiitd.ac.in>
2026-01-12 00:49:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Developing in progress by :</h1>
+      <h1>AK + Niharika</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
